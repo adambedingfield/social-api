b14a0c9bc4fc8ddcfc01cfa98addaf51e8575353
ACB <78452262+adambedingfield@users.noreply.github.com>
2021-06-20 08:24:23 -0700

    created thought routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
 // add /user prefix for user routes
 router.use('/users', userRoutes);
+// add /thoughts prefix for thought routes
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
